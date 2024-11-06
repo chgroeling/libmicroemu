9499a3b97a2d29d4b8d317317b808a440e9d9e70
Christian Gröling <contact@christiangroeling.de>
2024-11-06 04:49:39 +0100

    feature: Use Arg for StrBuilder in one case

--- src/libmicroemu/internal/utils/arg.h
+++ src/libmicroemu/internal/utils/arg.h
@@ -6,6 +6,7 @@
 
 template <microemu::RegisterId N> class ArgConst {
 public:
+  static constexpr auto kVal = N;
   constexpr microemu::RegisterId Get() const { return N; }
 
   /// \brief Constructor
@@ -29,6 +30,12 @@ public:
   /// \brief Move assignment operator for ArgConst.
   /// \param r_src the object to be copied
   constexpr ArgConst &operator=(ArgConst &&r_src) = default;
+
+  // Equality operator
+  bool operator==(const ArgConst &other) const { return kVal == other.kVal; }
+
+  // Inequality operator
+  bool operator!=(const ArgConst &other) const { return !(*this == other); }
 };
 
 template <typename T> class Arg {
@@ -56,6 +63,12 @@ public:
   Arg &operator=(Arg &&r_src) = default;
 
   inline microemu::RegisterId Get() const { return static_cast<microemu::RegisterId>(val_); }
+
+  // Equality operator
+  bool operator==(const Arg &other) const { return val_ == other.val_; }
+
+  // Inequality operator
+  bool operator!=(const Arg &other) const { return !(*this == other); }
 
 private:
   T val_;
