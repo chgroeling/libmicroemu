b14ea78b2d3f3730fffe1414a76ac58e06b885bb
Christian Gröling <contact@christiangroeling.de>
2024-11-08 05:41:51 +0100

    doc: Documented the processor states class

--- include/libmicroemu/processor_states.h
+++ include/libmicroemu/processor_states.h
@@ -13,10 +13,17 @@
 
 namespace microemu {
 
+/** @brief Processor states class.
+ *
+ * This class holds all the states necessary for emulating a processor.
+ * Mainly these are the registers, special registers and exception states.
+ */
 class ProcessorStates {
 
 public:
-  ProcessorStates() {
+  /** @brief Constructs a ProcessorStates object.
+   */
+  ProcessorStates() : registers_{}, special_registers_{}, exception_states_{} {
     registers_.fill(0U);
     special_registers_.fill(0U);
   };
@@ -32,13 +39,52 @@ public:
   /// \param r_src the object to be copied
   ProcessorStates(ProcessorStates &&r_src) noexcept = default;
 
+  /** @brief Get a reference on the raw register array.
+   *
+   * This function returns a reference to the raw register array.
+   *
+   * @return The raw register array.
+   */
   inline auto &GetRegisters() noexcept { return registers_; }
+
+  /** @brief Get a const reference on the raw register array.
+   *
+   * This function returns a const reference to the raw register array.
+   *
+   * @return The raw register array.
+   */
   inline const auto &GetRegisters() const noexcept { return registers_; }
 
+  /** @brief Get a reference on the raw special register array.
+   *
+   * This function returns a reference to the raw special register array.
+   *
+   * @return The raw special register array.
+   */
   inline auto &GetSpecialRegisters() noexcept { return special_registers_; }
+
+  /** @brief Get a const reference on the raw special register array.
+   *
+   * This function returns a const reference to the raw special register array.
+   *
+   * @return The raw special register array.
+   */
   inline const auto &GetSpecialRegisters() const noexcept { return special_registers_; }
 
+  /** @brief Get a reference on the exception states.
+   *
+   * This function returns a reference to the exception states.
+   *
+   * @return The exception states.
+   */
   inline auto &GetExceptionStates() noexcept { return exception_states_; }
+
+  /** @brief Get a const reference on the exception states.
+   *
+   * This function returns a const reference to the exception states.
+   *
+   * @return The exception states.
+   */
   inline const auto &GetExceptionStates() const noexcept { return exception_states_; }
 
 private:
