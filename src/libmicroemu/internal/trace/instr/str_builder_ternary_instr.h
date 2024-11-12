edadc8f1ea6684707f6932abf641c6c20c51dccb
Christian Gröling <contact@christiangroeling.de>
2024-11-12 06:03:56 +0100

    fix: Add missing U to flag evaluation in str builders

--- src/libmicroemu/internal/trace/instr/str_builder_ternary_instr.h
+++ src/libmicroemu/internal/trace/instr/str_builder_ternary_instr.h
@@ -18,7 +18,7 @@ public:
 
     const bool is_setflags = (iflags & static_cast<InstrFlagsSet>(InstrFlags::kSetFlags)) != 0U;
     const bool is_reduced_rd =
-        (bflags & static_cast<MnemonicBuilderFlagsSet>(MnemonicBuilderFlags::kReduceRd)) != 0;
+        (bflags & static_cast<MnemonicBuilderFlagsSet>(MnemonicBuilderFlags::kReduceRd)) != 0U;
     mctx.builder.AddString(instr_spec)
         .AddString(is_setflags == true ? "S" : "")
         .AddString(It::GetConditionAsStr(mctx.pstates))
